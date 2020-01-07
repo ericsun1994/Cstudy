e3ee091d643f1ad60c1c26dd6522bd7518c99ffb
선종수 <33773598+ericsun1994@users.noreply.github.com>
2020-01-07 17:25:34 +0900

    Create 01_search1.c

---
+++ algorithm/chap03/01_search1.c
@@ -0,0 +1,47 @@
+/* 2020-01-07 TUE
+ * 01_search.c: 선형 검색
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int search(const in a[], int n, int key)
+{
+    int i =0;
+    while(1)
+    {
+        if(i==n)
+            return -1;
+        if(a[i]==key)
+            return i;
+        i++;
+    }
+}
+
+int main(void)
+{
+    int i, nx, ky, idx;
+    int *x;
+
+    puts("선형 검색");
+    printf("요소 개수 : ");
+    scanf("%d", &nx);
+    x = calloc(nx, sizeof(int));
+    for(i=0; i<nx; i++)
+    {
+        printf("x[%d] : ", i);
+        scanf("%d", &x[i]);
+    }
+    printf("검색값 : ");
+    scanf("%d", &ky);
+    idx = search(x, nx, ky);
+    if(idx==1)
+        puts("검색에 실패했습니다.");
+    else
+    {
+        printf("%d(은)는 ㅌ[%d]에 있습니다.\n". ky, idx);
+    }
+    free(x);
+
+    return 0;
+}
